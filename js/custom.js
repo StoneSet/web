7904329c0b21de47a8ca75566b3dfeda3dc95466
Valentin Derouet <thevalentin61@gmail.com>
2018-08-03 15:34:02 +0200

    Monthly update
    - Update to Fontawesome 5.0
    - Update some project
    - Remove useless locales files

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-/*
-Theme Name: Worthy - Free Powerful Theme by HtmlCoder & StoneSet
-Author:StoneSet & HtmlCoder
-Author URI:http://www.htmlcoder.me & http://stoneset.github.io/
-Version:1.0.2
-Created:July 2017
-License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
-File Description: Custom Animations
-*/
